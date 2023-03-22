5a1eee5b9185790d3f2c2843314c4174d9676f14
Ignacio Cervantes <ignacio.m.cervantes@gmail.com>
2023-03-22 15:51:06 -0300

    Práctica 1 terminada

--- practica1.hs
+++ practica1.hs
@@ -1,3 +1,5 @@
+import Text.XHtml (p)
+
 -- Ejercicio 1:
 --A:
 
@@ -20,6 +22,11 @@ maxDelPar (a, b) =
         then a
         else b
 
+-- Ejercicio 2
+-- sucesor (sumar (maxDelPar (divisionYResto 10 5)) 9)
+-- sumar (maxDelPar (divisionYResto 25 5)) (sucesor 6)
+-- sumar (sucesor (maxDelPar (divisionYResto 49 7))) 4
+-- sucesor (sumar (maxDelPar (divisionYResto 36 6)) 5)
 
 --Tipos enumerativos
 -- Ejercicio 1:
@@ -143,19 +150,22 @@ data TipoDePoke = Agua | Fuego | Planta deriving Show
 
 data Pokemon = Poke TipoDePoke Int deriving Show
 
-data Entrenador = Ent String [Pokemon]
+data Entrenador = Ent String Pokemon Pokemon
 
 ash :: Entrenador
-ash = Ent "ash" [charmander, chicorita]
+ash = Ent "ash" charmander chicorita
 
 gary :: Entrenador
-gary = Ent "gary" [charmander, chicorita]
+gary = Ent "gary" charmander charmander
 
 charmander :: Pokemon
-charmander = Poke Fuego 100 
+charmander = Poke Fuego 100
 
 chicorita :: Pokemon
-chicorita = Poke Planta 100 
+chicorita = Poke Planta 100
+
+mudkip :: Pokemon
+mudkip = Poke Agua 100
 
 superaA :: Pokemon -> Pokemon -> Bool
 superaA (Poke t1 n1) (Poke t2 n2) = esEfectivo t1 t2
@@ -166,7 +176,60 @@ esEfectivo Fuego Planta = True
 esEfectivo Planta Agua = True
 esEfectivo a b = False
 
+cantidadDePokemonDe :: TipoDePoke -> Entrenador -> Int
+cantidadDePokemonDe t (Ent n p1 p2)
+  | sonDelMismoTipo t p1 p2 = 2
+  | unoEsDeTipo t p1 p2 = 1
+  | otherwise = 0
 
 
+unoEsDeTipo :: TipoDePoke -> Pokemon -> Pokemon -> Bool
+unoEsDeTipo t (Poke x e) (Poke z e2) =
+     esDeTipo t (Poke x e) || esDeTipo t (Poke z e2)
 
+sonDelMismoTipo :: TipoDePoke -> Pokemon -> Pokemon -> Bool
+sonDelMismoTipo t (Poke x e) (Poke z e2) =
+     esDeTipo t (Poke x e) && esDeTipo t (Poke z e2)
+
+tipoDe :: Pokemon -> TipoDePoke
+tipoDe (Poke t e) = t
+
+esDeTipo :: TipoDePoke -> Pokemon -> Bool
+esDeTipo Fuego (Poke Fuego e) = True
+esDeTipo Agua (Poke Agua k) = True
+esDeTipo Planta (Poke Planta j) = True
+esDeTipo x z = False
+
+
+juntarPokemon :: (Entrenador, Entrenador) -> [Pokemon]
+juntarPokemon (Ent n p1 p2, Ent n2 p3 p4) = [p1, p2, p3, p4]
+
+
+--5: Funciones polimorficas
+
+loMismo :: a -> a
+loMismo a = a
+
+siempreSiete :: a -> Int
+siempreSiete a = 7
+
+swap :: (a, b) -> (b, a)
+swap (a, b) = (b, a)
+
+-- Las funciones son polimorficas porque les importa la estructura, pero no el tipo de dato
+
+-- Ejercicio 6
+
+estaVacia :: [a] -> Bool
+estaVacia [] = True
+estaVacia _ = False
+
+elPrimero :: [a] -> a
+elPrimero (x:_) = x
+
+sinElPrimero :: [a] -> [a]
+sinElPrimero (x:a) = a
+
+splitHead :: [a] -> (a, [a])
+splitHead (x:a) = (x, a)
 
